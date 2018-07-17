3b880282dd5fac1aeba166e44615f39a766ad23b
Errlin Deleon <EDeleon@student.neumont.edu>
2018-07-17 10:01:35 -0600

    Kill Me

--- CanvasDrawDemo/app/src/main/java/com/example/canvasdrawdemo/CanvasView.java
+++ CanvasDrawDemo/app/src/main/java/com/example/canvasdrawdemo/CanvasView.java
@@ -1,14 +1,35 @@
 package com.example.canvasdrawdemo;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.ContextWrapper;
+import android.content.DialogInterface;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.media.MediaScannerConnection;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.ImageView;
+
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Random;
+import java.util.UUID;
 
 // Code from https://examples.javacodegeeks.com/android/core/graphics/canvas-graphics/android-canvas-example/
 
@@ -80,6 +101,17 @@ public class CanvasView extends View {
         mPath.reset();
         invalidate();
     }
+
+    public void saveCanvas(String image_name) {
+        String imgSaved = MediaStore.Images.Media.insertImage(
+                context.getContentResolver(), ,
+                UUID.randomUUID().toString()+".jpeg", "drawing");
+    }
+
+    public void loadCanvas(){
+
+    }
+
 
     // when ACTION_UP stop touch
     private void upTouch() {
--- CanvasDrawDemo/app/src/main/java/com/example/canvasdrawdemo/MainActivity.java
+++ CanvasDrawDemo/app/src/main/java/com/example/canvasdrawdemo/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.canvasdrawdemo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 // Code from https://examples.javacodegeeks.com/android/core/graphics/canvas-graphics/android-canvas-example/
 
@@ -21,5 +22,19 @@ public class MainActivity extends AppCompatActivity {
 
     public void clearCanvas(View v) {
         customCanvas.clearCanvas();
+    }
+
+    public void saveCanvas(View v) {
+        if(v.getId() == R.id.saveButton) {
+            customCanvas.saveCanvas("image1");
+            Toast.makeText(getApplicationContext(), "Image has been saved", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public void loadCanvas(View v) {
+        if(v.getId() == R.id.loadButton) {
+            customCanvas.loadCanvas();
+            Toast.makeText(getApplicationContext(), "Image has been loaded", Toast.LENGTH_LONG).show();
+        }
     }
 }
